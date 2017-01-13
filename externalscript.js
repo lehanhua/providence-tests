24db47066168413dbf54efc7ddc93e1a74aa335b
Lehan <lehan.huang@salesforce.com>
2017-01-12 16:29:14 -0800

    modify

--- externalscript.js
+++ externalscript.js
@@ -1,3 +1,3 @@
 var script = 'https://www.evil.com/script.js';
-//var script = 'https://www.evil.com/script.js';
-var script2 = 'https://www.evil.com/script2.js';
+//var script2 = 'http://www.evil.com/script.js';
+var script2 = 'http://www.evil.com/script2.js';
